b5a04159818d054749febeff1df5239840f2e26c
Tim Poot <tpoot96@gmail.com>
2019-10-13 18:06:52 +0200

    Revert "Finalize text version"
    This reverts commit 71a67d5a7222d8e147f34096d77f77f424060871.

--- main.py
+++ main.py
@@ -1,122 +1,75 @@
-Python 3.6.0 (v3.6.0:41df79263a11, Dec 23 2016, 07:18:10) [MSC v.1900 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> 
- RESTART: C:\Users\Tim\Documents\Programming\Python\Elementary-Cellular-Automata\main.py 
-X X _ X _ _ X X X _ _ X X _ _ X _ X X _ X X _ _ X 
-Traceback (most recent call last):
-  File "C:\Users\Tim\Documents\Programming\Python\Elementary-Cellular-Automata\main.py", line 75, in <module>
-    automata.run(50)
-  File "C:\Users\Tim\Documents\Programming\Python\Elementary-Cellular-Automata\main.py", line 57, in run
-    self.updateStrip()
-  File "C:\Users\Tim\Documents\Programming\Python\Elementary-Cellular-Automata\main.py", line 48, in updateStrip
-    bufferstrip[i] = int(self.dictionaryRule[self.getNeighbourhood(i)])
-  File "C:\Users\Tim\Documents\Programming\Python\Elementary-Cellular-Automata\main.py", line 41, in getNeighbourhood
-    right = self.strip[i + 1]
-IndexError: list index out of range
->>> 
- RESTART: C:\Users\Tim\Documents\Programming\Python\Elementary-Cellular-Automata\main.py 
-X X X _ X _ X X _ _ X _ X X X _ _ _ X X X X X _ X 
-_ _ X X X X X X _ X X X X _ X _ _ X X _ _ _ X X X 
-_ X X _ _ _ _ X X X _ _ X X X _ X X X _ _ X X _ X 
-X X X _ _ _ X X _ X _ X X _ X X X _ X _ X X X X X 
-_ _ X _ _ X X X X X X X X X X _ X X X X X _ _ _ _ 
-_ X X _ X X _ _ _ _ _ _ _ _ X X X _ _ _ X _ _ _ _ 
-X X X X X X _ _ _ _ _ _ _ X X _ X _ _ X X _ _ _ _ 
-X _ _ _ _ X _ _ _ _ _ _ X X X X X _ X X X _ _ _ X 
-X _ _ _ X X _ _ _ _ _ X X _ _ _ X X X _ X _ _ X X 
-X _ _ X X X _ _ _ _ X X X _ _ X X _ X X X _ X X _ 
-X _ X X _ X _ _ _ X X _ X _ X X X X X _ X X X X X 
-X X X X X X _ _ X X X X X X X _ _ _ X X X _ _ _ _ 
-X _ _ _ _ X _ X X _ _ _ _ _ X _ _ X X _ X _ _ _ X 
-X _ _ _ X X X X X _ _ _ _ X X _ X X X X X _ _ X X 
-X _ _ X X _ _ _ X _ _ _ X X X X X _ _ _ X _ X X _ 
-X _ X X X _ _ X X _ _ X X _ _ _ X _ _ X X X X X X 
-X X X _ X _ X X X _ X X X _ _ X X _ X X _ _ _ _ _ 
-X _ X X X X X _ X X X _ X _ X X X X X X _ _ _ _ X 
-X X X _ _ _ X X X _ X X X X X _ _ _ _ X _ _ _ X X 
-_ _ X _ _ X X _ X X X _ _ _ X _ _ _ X X _ _ X X _ 
-_ X X _ X X X X X _ X _ _ X X _ _ X X X _ X X X _ 
-X X X X X _ _ _ X X X _ X X X _ X X _ X X X _ X _ 
-X _ _ _ X _ _ X X _ X X X _ X X X X X X _ X X X X 
-X _ _ X X _ X X X X X _ X X X _ _ _ _ X X X _ _ _ 
-X _ X X X X X _ _ _ X X X _ X _ _ _ X X _ X _ _ X 
-X X X _ _ _ X _ _ X X _ X X X _ _ X X X X X _ X X 
-_ _ X _ _ X X _ X X X X X _ X _ X X _ _ _ X X X _ 
-_ X X _ X X X X X _ _ _ X X X X X X _ _ X X _ X _ 
-X X X X X _ _ _ X _ _ X X _ _ _ _ X _ X X X X X _ 
-X _ _ _ X _ _ X X _ X X X _ _ _ X X X X _ _ _ X X 
-X _ _ X X _ X X X X X _ X _ _ X X _ _ X _ _ X X _ 
-X _ X X X X X _ _ _ X X X _ X X X _ X X _ X X X X 
-X X X _ _ _ X _ _ X X _ X X X _ X X X X X X _ _ _ 
-X _ X _ _ X X _ X X X X X _ X X X _ _ _ _ X _ _ X 
-X X X _ X X X X X _ _ _ X X X _ X _ _ _ X X _ X X 
-_ _ X X X _ _ _ X _ _ X X _ X X X _ _ X X X X X _ 
-_ X X _ X _ _ X X _ X X X X X _ X _ X X _ _ _ X _ 
-X X X X X _ X X X X X _ _ _ X X X X X X _ _ X X _ 
-X _ _ _ X X X _ _ _ X _ _ X X _ _ _ _ X _ X X X X 
-X _ _ X X _ X _ _ X X _ X X X _ _ _ X X X X _ _ _ 
-X _ X X X X X _ X X X X X _ X _ _ X X _ _ X _ _ X 
-X X X _ _ _ X X X _ _ _ X X X _ X X X _ X X _ X X 
-_ _ X _ _ X X _ X _ _ X X _ X X X _ X X X X X X _ 
-_ X X _ X X X X X _ X X X X X _ X X X _ _ _ _ X _ 
-X X X X X _ _ _ X X X _ _ _ X X X _ X _ _ _ X X _ 
-X _ _ _ X _ _ X X _ X _ _ X X _ X X X _ _ X X X X 
-X _ _ X X _ X X X X X _ X X X X X _ X _ X X _ _ _ 
-X _ X X X X X _ _ _ X X X _ _ _ X X X X X X _ _ X 
-X X X _ _ _ X _ _ X X _ X _ _ X X _ _ _ _ X _ X X 
-_ _ X _ _ X X _ X X X X X _ X X X _ _ _ X X X X _ 
-_ X X _ X X X X X _ _ _ X X X _ X _ _ X X _ _ X _ 
->>> 
- RESTART: C:\Users\Tim\Documents\Programming\Python\Elementary-Cellular-Automata\main.py 
-X X X X _ X X X X X X _ X X _ X X X X X _ _ _ _ X X _ X _ X 
-_ _ _ X X X _ _ _ _ X X X X X X _ _ _ X _ _ _ X X X X X X X 
-_ _ X X _ X _ _ _ X X _ _ _ _ X _ _ X X _ _ X X _ _ _ _ _ X 
-_ X X X X X _ _ X X X _ _ _ X X _ X X X _ X X X _ _ _ _ X X 
-X X _ _ _ X _ X X _ X _ _ X X X X X _ X X X _ X _ _ _ X X X 
-_ X _ _ X X X X X X X _ X X _ _ _ X X X _ X X X _ _ X X _ _ 
-X X _ X X _ _ _ _ _ X X X X _ _ X X _ X X X _ X _ X X X _ _ 
-X X X X X _ _ _ _ X X _ _ X _ X X X X X _ X X X X X _ X _ X 
-_ _ _ _ X _ _ _ X X X _ X X X X _ _ _ X X X _ _ _ X X X X X 
-_ _ _ X X _ _ X X _ X X X _ _ X _ _ X X _ X _ _ X X _ _ _ X 
-_ _ X X X _ X X X X X _ X _ X X _ X X X X X _ X X X _ _ X X 
-_ X X _ X X X _ _ _ X X X X X X X X _ _ _ X X X _ X _ X X X 
-X X X X X _ X _ _ X X _ _ _ _ _ _ X _ _ X X _ X X X X X _ X 
-_ _ _ _ X X X _ X X X _ _ _ _ _ X X _ X X X X X _ _ _ X X X 
-_ _ _ X X _ X X X _ X _ _ _ _ X X X X X _ _ _ X _ _ X X _ X 
-_ _ X X X X X _ X X X _ _ _ X X _ _ _ X _ _ X X _ X X X X X 
-_ X X _ _ _ X X X _ X _ _ X X X _ _ X X _ X X X X X _ _ _ X 
-X X X _ _ X X _ X X X _ X X _ X _ X X X X X _ _ _ X _ _ X X 
-_ _ X _ X X X X X _ X X X X X X X X _ _ _ X _ _ X X _ X X _ 
-_ X X X X _ _ _ X X X _ _ _ _ _ _ X _ _ X X _ X X X X X X _ 
-X X _ _ X _ _ X X _ X _ _ _ _ _ X X _ X X X X X _ _ _ _ X _ 
-X X _ X X _ X X X X X _ _ _ _ X X X X X _ _ _ X _ _ _ X X X 
-_ X X X X X X _ _ _ X _ _ _ X X _ _ _ X _ _ X X _ _ X X _ _ 
-X X _ _ _ _ X _ _ X X _ _ X X X _ _ X X _ X X X _ X X X _ _ 
-X X _ _ _ X X _ X X X _ X X _ X _ X X X X X _ X X X _ X _ X 
-_ X _ _ X X X X X _ X X X X X X X X _ _ _ X X X _ X X X X X 
-X X _ X X _ _ _ X X X _ _ _ _ _ _ X _ _ X X _ X X X _ _ _ X 
-_ X X X X _ _ X X _ X _ _ _ _ _ X X _ X X X X X _ X _ _ X X 
-X X _ _ X _ X X X X X _ _ _ _ X X X X X _ _ _ X X X _ X X X 
-_ X _ X X X X _ _ _ X _ _ _ X X _ _ _ X _ _ X X _ X X X _ _ 
-X X X X _ _ X _ _ X X _ _ X X X _ _ X X _ X X X X X _ X _ _ 
-X _ _ X _ X X _ X X X _ X X _ X _ X X X X X _ _ _ X X X _ X 
-X _ X X X X X X X _ X X X X X X X X _ _ _ X _ _ X X _ X X X 
-X X X _ _ _ _ _ X X X _ _ _ _ _ _ X _ _ X X _ X X X X X _ _ 
-X _ X _ _ _ _ X X _ X _ _ _ _ _ X X _ X X X X X _ _ _ X _ X 
-X X X _ _ _ X X X X X _ _ _ _ X X X X X _ _ _ X _ _ X X X X 
-_ _ X _ _ X X _ _ _ X _ _ _ X X _ _ _ X _ _ X X _ X X _ _ _ 
-_ X X _ X X X _ _ X X _ _ X X X _ _ X X _ X X X X X X _ _ _ 
-X X X X X _ X _ X X X _ X X _ X _ X X X X X _ _ _ _ X _ _ _ 
-X _ _ _ X X X X X _ X X X X X X X X _ _ _ X _ _ _ X X _ _ X 
-X _ _ X X _ _ _ X X X _ _ _ _ _ _ X _ _ X X _ _ X X X _ X X 
-X _ X X X _ _ X X _ X _ _ _ _ _ X X _ X X X _ X X _ X X X _ 
-X X X _ X _ X X X X X _ _ _ _ X X X X X _ X X X X X X _ X X 
-_ _ X X X X X _ _ _ X _ _ _ X X _ _ _ X X X _ _ _ _ X X X _ 
-_ X X _ _ _ X _ _ X X _ _ X X X _ _ X X _ X _ _ _ X X _ X _ 
-X X X _ _ X X _ X X X _ X X _ X _ X X X X X _ _ X X X X X _ 
-X _ X _ X X X X X _ X X X X X X X X _ _ _ X _ X X _ _ _ X X 
-X X X X X _ _ _ X X X _ _ _ _ _ _ X _ _ X X X X X _ _ X X _ 
-X _ _ _ X _ _ X X _ X _ _ _ _ _ X X _ X X _ _ _ X _ X X X X 
-X _ _ X X _ X X X X X _ _ _ _ X X X X X X _ _ X X X X _ _ _ 
-X _ X X X X X _ _ _ X _ _ _ X X _ _ _ _ X _ X X _ _ X _ _ X 
->>> 
+import random
+
+class ElementaryCellularAutomata:
+
+    #initialize instance, takes lengt of strip as int and a Wolfram Rule as int
+    #length is length of the strip
+    #both Rule variables are representations of the Wolfram rule, decimal in decimal int format
+    #and dictionary as a 3 bitstring -> T/F mapping
+    #strip is a list of integers, range(0, 1)
+    def __init__(self, length, rule):
+        self.length = length
+        self.decimalRule = rule
+        self.dictionaryRule = dict()
+
+        self.strip = self.getRandomStrip()
+        self.initRule()
+
+    def getRandomStrip(self):
+        randomStrip = list()
+        for i in range(0, self.length):
+            randomStrip.append(random.randint(0, 1)) 
+        return randomStrip
+
+    #initializes a rule dictionary from the decimal rule
+    #format {'101' = '0', ... , '11'='1'}
+    def initRule(self):
+        binaryRule = self.toBinaryString(self.decimalRule)
+        dictionaryKey =  7
+
+        #prepend 0s for full dictionary
+        while len(binaryRule) < 8:
+            binaryRule = "0" + binaryRule
+        #build dictionary
+        for c in binaryRule:
+            self.dictionaryRule[self.toBinaryString(dictionaryKey)] = c
+            dictionaryKey -= 1
+
+    def getNeighbourhood(self, i):
+        left = self.strip[i - 1]
+        center = self.strip[i]
+        right = self.strip[i + 1]
+        neighbourhood = 100 * left + 10 * center + right
+        return str(neighbourhood)
+
+    def updateStrip(self):
+        bufferstrip = self.strip
+        for i in range(0, self.length):
+            bufferstrip[i] = dictionaryRule[getNeighbourhood(i)]
+        self.strip = bufferstrip          
+
+    def toBinaryString(self, x):
+        return str(bin(x).replace("0b", ""))
+
+    def run(self, n):
+        self.printStrip()
+        for i in range(0, n)
+            self.updateStrip()
+            self.printStrip()
+    
+    def printStrip(self):
+        for i in range(0, self.length):
+            if self.strip[i]:
+                print('X', end='', flush=True)
+            else:
+                print('_', end='', flush=True)
+        print()
+
+    #used for debugging
+    def testPrint(self):
+        print("Strip: " + str(self.strip))
+        print("Decimal rule: " + str(self.decimalRule))
+        print("dictionaryRule: " + str(self.dictionaryRule))
+
+automata = ElementaryCellularAutomata(10, 3)
+automata.testPrint()
