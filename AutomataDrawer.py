1cd76d61919737191826e26e88162c0f148e2b0d
Tim Poot <tpoot96@gmail.com>
2019-10-14 12:27:40 +0200

    Added automata drawer that can draw lines of a strip and export the image

--- AutomataDrawer.py
+++ AutomataDrawer.py
@@ -1,5 +1,6 @@
 from PIL import Image, ImageDraw
 import math
+import random
 
 class AutomataDrawer:
 	#takes the length of strip and the amount of iterations that the automata runs for
@@ -7,12 +8,26 @@ class AutomataDrawer:
 		self.stripLength = stripLength
 		self.iterations = iterations
 		self.canvas = Image.new("L", (1000, 1000))
+		self.draw = ImageDraw.Draw(self.canvas)
 
-		self.cellWidth = 10
-		self.cellHeight = 10
+		self.draw.rectangle([(0,0), (1000,1000)], 255)
+
+		self.cellWidth = 25
+		self.cellHeight = 25
 		if (self.cellWidth * self.stripLength) > 1000:
 			self.cellWidth = math.floor(1000 / self.stripLength)
 		if (self.cellHeight * self.iterations) > 1000:
 			self.cellHeight = math.floor(1000 / self.iterations)
 
-		print("You now have a drawer with a stripLength of %s and a #iterations equal to %s !" % (self.stripLength, self.iterations))
+	#draws one line from a strip (bitstring) and an offset for the height which is equal 
+	#to the amount of iterations that have passed.  
+	def drawLine(self, strip, offsetHeight):
+		for index, cell in enumerate(strip):
+			if cell:
+				x0, y0 = index * self.cellWidth, offsetHeight * self.cellHeight
+				x1, y1 = (index * self.cellWidth) + self.cellWidth, (offsetHeight * self.cellHeight) + self.cellHeight
+				self.draw.rectangle([(x0, y0), (x1, y1)], 0)
+
+	#takes the rule as string and saves the image as png with the rule as the filename
+	def saveImage(self, rule):
+		self.canvas.save(rule + ".png")
